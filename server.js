5b59cd482ca7d0c535b3954fba52f281ededa339
randilfernando <randil.fernando@outlook.com>
2018-02-07 00:09:30 +0530

    api.roouter error fixed

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+
+connection.connect();
 const apiRouter = require('./app/api.router');
 const viewRouter = require('./app/view.router');
 
@@ -10,15 +11,6 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'RAnd12!@',
-    database: 'boresy'
-});
-
-connection.connect();
 
 app.use('/api', apiRouter);
 app.use('/view', viewRouter);
