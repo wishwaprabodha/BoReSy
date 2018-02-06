8eb5f4a4c79b7776557460f51ea2d89018c7920d
wishwa <ww46673@gmail.com>
2018-02-06 13:17:34 -0800

    search boarding

--- app/api.router.js
+++ app/api.router.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'RAnd12!@',
+    password: '',
     database: 'boresy'
 });
 
@@ -73,6 +73,30 @@ apiRouter.post('/add-boarding', function (req, res) {
         } else {
             res.send({
                 success: true
+            })
+        }
+    });
+})
+
+
+apiRouter.post('/search-boarding', function (req, res) {
+    connection.query('SELECT * FROM place WHERE rental BETWEEN ? AND ? AND gender=?', [
+        req.body['min_rental'],
+        req.body['max_rental'],
+        req.body['gender'],
+    ], function (error, results, fields) {
+        results = JSON.parse(JSON.stringify(results));
+        //console.log(results);
+        res.status(200);
+        if (error) {
+            console.error(error);
+            res.send({
+                success: false
+            })
+        } else {
+            res.send({
+                success: true,
+                data: results
             })
         }
     });
